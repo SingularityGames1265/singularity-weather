4b145e52098aac94cdb5a877e18cc777591c46cd
SingularityGames1265 <vanbaarjerry@gmail.com>
2026-06-22 16:06:14 +0200

    Updated cursor trail

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ const hasLoadedBefore = sessionStorage.getItem(pageKey);
     const spread = 10;
     const offsetX = (Math.random() - 0.5) * spread;
     const offsetY = (Math.random() - 0.5) * spread;
-    const rotation = Math.random() * 360;
 
     trail.style.left = (e.clientX + offsetX) + 'px';
     trail.style.top = (e.clientY + offsetY) + 'px';
